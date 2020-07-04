b294d7cf6112ccd7060b80cb1f24fc999b4a391e
Jzavier Timm <jztimm@gmail.com>
2020-07-04 21:30:30 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -46,14 +46,14 @@ def sorted_matrix
   # However, this time, sort each internal array alphabetically by the first character
   
   matrix = [
-    CONVENTIONAL_PRODUCE = [
+    CONVENTIONAL_PRODUCE.sort = [
   "Grapefruit",
   "Pineapple",
   "Oranges",
   "Watermelon",
   "Eggplant"],
 
-    ORGANIC_PRODUCE = [
+    ORGANIC_PRODUCE.sort = [
   "Strawberries",
   "Potatoes",
   "Grapes",
